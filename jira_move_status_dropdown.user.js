b6bf782c63440c1e170c6b79183c3589c95863d5
Jacob Mack <saiori@gmail.com>
2025-09-26 16:33:20 -0700

    Update jira_move_status_dropdown.user.js

--- jira_move_status_dropdown.user.js
+++ jira_move_status_dropdown.user.js
@@ -1,1 +1,65 @@
+// ==UserScript==
+// @name         Re-Arrange Quick Filter Links on the Gantt
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       @saiori
+// @updateURL    https://github.com/saiori/userscripts/blob/master/jira_move_status_dropdown.user.js
+// @downloadURL  https://github.com/saiori/userscripts/blob/master/jira_move_status_dropdown.user.js
+// @match        https://lulusdev.atlassian.net/browse/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.net
+// @grant        GM_addStyle
+// ==/UserScript==
+
+function setDeceleratingTimeout(callback, milliseconds, factor, maximum)
+{
+    if (factor == undefined)
+    {
+        factor = milliseconds;
+    }
+
+    if (maximum == undefined)
+    {
+        maximum = milliseconds * 100
+    }
+
+    var internalCallback = function(tick, counter) {
+        return function() {
+            if (--tick >= 0) {
+                // default to linear growth, if a number greater than 1 given
+                let interval = ++counter * factor + milliseconds;
+                if (factor > 0 && factor <= 1) {
+                    interval = milliseconds * Math.pow(2 - factor, ++counter);
+                }
+
+                interval = Math.min(interval, maximum || interval);
+                // console.log(`Checking... next check in ${interval}ms`);
+                window.setTimeout(internalCallback, interval);
+                callback();
+            }
+        }
+    }(milliseconds, 0);
+
+    window.setTimeout(internalCallback, factor);
+};
+
+function moveStatusDropdown() {
+    const statusSelectorDataId = 'ref-spotlight-target-status-and-approval-spotlight';
+    let statusSelector = document.querySelector('[data-testid="' + statusSelectorDataId + '"]');
+    
+    const headerSelectorDataId = 'issue-view-sticky-header-container.sticky-header';
+    let headerSelector = document.querySelector('[data-testid="' + headerSelectorDataId + '"] nav');
+    
+    
+    if (typeof statusSelector === 'null' || typeof headerrSelector === 'null') {
+        return;
+    }
+    
+    headerSelector.appendChild(statusSelector);
+}
+
+
+setDeceleratingTimeout(function() {
+    reorderQuickFilterButtons()
+}, 8000, .7);
 
